646aeb772a08760146c9abc08023d2119b594a48
Adeshkumarshrivastva <139764318+Adeshkumarshrivastva@users.noreply.github.com>
2025-05-01 13:49:12 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx',
-    },
-  },
-});
+  plugins: [react()],
+})
